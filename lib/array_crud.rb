2f9a299971adc79bb6e3cfb38e59b2e00ff0858f
fortuitous-dispatch-6424 <tanisha_henry28@hotmail.com>
2019-08-17 05:03:45 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,6 @@ end
 def #add_element_to_end_of_array
   array << element
 end
-
 def #add_element_to_start_of_array
   array.unshift(element)
 end  
@@ -18,15 +17,12 @@ end
 def #remove_element_from_start_of_array(array)
   array.shift
 end
-
 def #retrieve_element_from_index(array, index_number)
   array[index_number]
 end
-
 def #retrieve_first_element_from_array(array)
    array[0]
 end
-
 def #retrieve_last_element_from_array(array)
   x = array.count
   array[x-1]
