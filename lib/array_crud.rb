4001235b0a5999ccbeff1a31bd979b7badf06606
fortuitous-dispatch-6424 <tanisha_henry28@hotmail.com>
2019-08-17 04:13:57 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,29 +1,26 @@
-def create_an_empty_array
-  
-end
-
-def create_an_array
-  
-end
-
-def add_element_to_end_of_array(array, element)
-  
+def
+  array = ["wow","I","am","really","learning"]
+  element = "arrays!"
+  end
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  array = ["I", "am", "really", "learning"]
+  element = "wow"
 end
 
 def remove_element_from_end_of_array(array)
+ 
   
 end
 
 def remove_element_from_start_of_array(array)
+
   
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+ 
 end
 
 def retrieve_first_element_from_array(array)
