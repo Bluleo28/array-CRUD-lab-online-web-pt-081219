2b5c5fd23a7f4e5915de7577ce970b80d2c6a421
fortuitous-dispatch-6424 <tanisha_henry28@hotmail.com>
2019-08-17 05:17:42 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,14 +1,14 @@
 def create_an_empty_array
-[]
+    []
 end
 def create_an_array
-["red","blue","gold","black"]
+    ["red","blue","gold","black"]
 end  
 def add_element_to_end_of_array
-array << element
+    array << element
 end
-def add_element_to_start_of_array
-  array.unshift (element)
+    def add_element_to_start_of_array
+array.unshift (element)
 end  
 def remove_element_from_end_of_array(array)
   array.pop
