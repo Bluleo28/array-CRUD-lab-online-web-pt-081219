518bb51c82046cff6402efcebcb8d835a7aa1011
fortuitous-dispatch-6424 <tanisha_henry28@hotmail.com>
2019-08-17 05:20:27 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -5,7 +5,7 @@ def create_an_array
     ["red","blue","gold","black"]
 end  
 def add_element_to_end_of_array
-    array << element
+  array << element
 end
 def add_element_to_start_of_array
   array.unshift (element)
