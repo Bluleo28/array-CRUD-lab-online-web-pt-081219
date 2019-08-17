6a8911975537287c7035e47d086693dde300772d
fortuitous-dispatch-6424 <tanisha_henry28@hotmail.com>
2019-08-17 04:54:20 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -28,7 +28,8 @@ end
 def #retrieve_element_from_index(array, index_number)
   array=  ["wow","I", "am", "really","learning", "arrays!"]
   index_number = 2
-end
+  end
+end  
 
 def #retrieve_first_element_from_array(array)
    array = ["I","am","really","learning","arrays!"] 
